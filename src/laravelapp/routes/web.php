29f98371fdc0230814c69295c04306d4d44c672b
hurugi7 <r.f.pockysakusaku@gmail.com>
2022-12-12 00:15:45 +0900

    URIの変更
    適切に設計されていなかったため

--- src/laravelapp/routes/web.php
+++ src/laravelapp/routes/web.php
@@ -22,40 +22,40 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 Route::controller(HaveCategoryController::class)->group(function () {
-    Route::get('/have/category/{category}, index')->name('have_category.index');
-    Route::get('/have/category/create, create')->name('have_category.create');
-    Route::post('/have/category/create, store')->name('have_category.store');
-    Route::get('/have/category/{category}, edit')->name('have_category.edit');
-    Route::put('/have/category/{category}, update')->name('have_category.update');
-    Route::delete('/have/category/{category}, destroy')->name('have_category.destroy');
+    Route::get('/have/categories, index')->name('have_category.index');
+    Route::get('/have/categories/create, create')->name('have_category.create');
+    Route::post('/have/categories/create, store')->name('have_category.store');
+    Route::get('/have/categories/{category}/edit, edit')->name('have_category.edit');
+    Route::put('/have/categories/{category}, update')->name('have_category.update');
+    Route::delete('/have/categories/{category}, destroy')->name('have_category.destroy');
 });
 
 Route::controller(WantCategoryController::class)->group(function () {
-    Route::get('/want/category/{category}, index')->name('want_category.index');
-    Route::get('/want/category/{category}, create')->name('want_category.create');
-    Route::post('/want/category/{category}, store')->name('want_category.store');
-    Route::get('/want/category/{category}, edit')->name('want_category.edit');
-    Route::put('/want/category/{category}, update')->name('want_category.update');
-    Route::delete('/want/category/{category}, destroy')->name('want_category.destroy');
+    Route::get('/want/categories, index')->name('want_category.index');
+    Route::get('/want/categories/crete, create')->name('want_category.create');
+    Route::post('/want/categories/create, store')->name('want_category.store');
+    Route::get('/want/categories/{category}/edit, edit')->name('want_category.edit');
+    Route::put('/want/categories/{category}, update')->name('want_category.update');
+    Route::delete('/want/categories/{category}, destroy')->name('want_category.destroy');
 });
 
 
 Route::controller(HaveSubCategoryController::class)->group(function () {
-    Route::get('/have/category/{category}/sub_category/{sub_category}, index')->name('have_sub_category.index');
-    Route::get('/have/category/{category}/sub_category/{sub_category}, create')->name('have_sub_category.create');
-    Route::post('/have/category/{category}/sub_category/{sub_category}, store')->name('have_sub_category.store');
-    Route::get('/have/category/{category}/sub_category/{sub_category}, edit')->name('have_sub_category.edit');
-    Route::put('/have/category/{category}/sub_category/{sub_category}, update')->name('have_sub_category.update');
-    Route::delete('/have/category/{category}/sub_category/{sub_category}, destroy')->name('have_sub_category.destroy');
+    Route::get('/have/categories/{category}/sub_categories, index')->name('have_sub_category.index');
+    Route::get('/have/categories/{category}/sub_categories/create, create')->name('have_sub_category.create');
+    Route::post('/have/categories/{category}/sub_categories/create, store')->name('have_sub_category.store');
+    Route::get('/have/categories/{category}/sub_categories/{sub_category}/edit, edit')->name('have_sub_category.edit');
+    Route::put('/have/categories/{category}/sub_categories/{sub_category}, update')->name('have_sub_category.update');
+    Route::delete('/have/categories/{category}/sub_categories/{sub_category}, destroy')->name('have_sub_category.destroy');
 });
 
 Route::controller(WantSubCategoryController::class)->group(function () {
-    Route::get('/want/category/{category}/sub_category/{sub_category}, index')->name('want_sub_category.index');
-    Route::get('/want/category/{category}/sub_category/{sub_category}, create')->name('want_sub_category.create');
-    Route::post('/want/category/{category}/sub_category/{sub_category}, store')->name('want_sub_category.store');
-    Route::get('/want/category/{category}/sub_category/{sub_category}, edit')->name('want_sub_category.edit');
-    Route::put('/want/category/{category}/sub_category/{sub_category}, update')->name('want_sub_category.update');
-    Route::delete('/want/category/{category}/sub_category/{sub_category}, destroy')->name('want_sub_category.destroy');
+    Route::get('/want/categories/{category}/sub_categories, index')->name('want_sub_category.index');
+    Route::get('/want/categories/{category}/sub_categories/create, create')->name('want_sub_category.create');
+    Route::post('/want/categories/{category}/sub_categories/create, store')->name('want_sub_category.store');
+    Route::get('/want/categories/{category}/sub_categories/{sub_category}/edit, edit')->name('want_sub_category.edit');
+    Route::put('/want/categories/{category}/sub_categories/{sub_category}, update')->name('want_sub_category.update');
+    Route::delete('/want/categories/{category}/sub_categories/{sub_category}, destroy')->name('want_sub_category.destroy');
 });
 
 require __DIR__.'/auth.php';
