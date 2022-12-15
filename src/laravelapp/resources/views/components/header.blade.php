20483c9cf8c737580629a50dcf41bdb0b1fd1a78
hurugi7 <r.f.pockysakusaku@gmail.com>
2022-12-16 02:09:37 +0900

    ヘッダー用のテンプレートファイルを新規追加

---
+++ src/laravelapp/resources/views/components/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+  <nav class="p-4 flex items-center flex-wrap bg-teal-500">
+    <div class="flex items-center shrink-0 text-black mr-4">
+      <span class="font-semibold text-xl">monogra</span>
+    </div>
+  </nav>
+</header>
---
+++ src/laravelapp/resources/views/have_category_index.blade.php
@@ -0,0 +1,3 @@
+<x-guest-layout>
+  <x-header/>
+</x-guest-layout>
