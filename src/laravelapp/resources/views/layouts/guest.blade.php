237c2d9b9616c3858a501b266fe96e399fd1361b
hurugi7 <r.f.pockysakusaku@gmail.com>
2022-12-20 01:42:27 +0900

    フォントの変更
    日本語が読みづらかったため

--- src/laravelapp/resources/views/layouts/guest.blade.php
+++ src/laravelapp/resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="font-note text-gray-900 antialiased">
             {{ $slot }}
         </div>
     </body>
