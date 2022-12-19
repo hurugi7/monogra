fd66437fc97ea884ad616a1dca0460b718f32efe
hurugi7 <r.f.pockysakusaku@gmail.com>
2022-12-20 01:47:00 +0900

    検索ボックス、ユーザーアイコンの追加。また、各アイテムの位置調整。

--- src/laravelapp/resources/views/components/header.blade.php
+++ src/laravelapp/resources/views/components/header.blade.php
@@ -1,27 +1,39 @@
-<header>
-  <nav class="p-4 flex items-center flex-wrap bg-teal-500">
-    <div class="flex items-center shrink-0 text-black">
-      <span class="font-semibold text-xl">monogra</span>
-    </div>
+<nav class="p-4 flex items-center flex-wrap bg-gray-300 justify-start gap-8">
+  <div class="items-center shrink-0 text-black mr-10">
+    <span class="font-semibold text-xl">monogra</span>
+  </div>
 
-    <x-dropdown>
-      <x-slot name="trigger">
-        <span class="font-semibold text-xl">持っているモノ</span>
-        <i class="fa fa-caret-down"></i>
-      </x-slot>
-      <x-slot name="content">
-        <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">持っているモノ</a>
-        <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">欲しいモノ</a>
-      </x-slot>
-    </x-dropdown>
+  <x-dropdown>
+    <x-slot name="trigger">
+      <span class="font-semibold text-xl">持っているモノ</span>
+      <i class="fa fa-caret-down"></i>
+    </x-slot>
+    <x-slot name="content">
+      <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">持っているモノ</a>
+      <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">欲しいモノ</a>
+    </x-slot>
+  </x-dropdown>
 
-    <form action="#" method="get" class="mr-4">
-      <input type="search" name="search" placeholder="モノを検索">
-      <button type="submit">検索</button>
-    </form>
+  <form action="#" method="get" class="ml-auto">
+    <input type="search" name="search" placeholder="モノを検索" class="rounded-md">
+    <button class="shadow-lg px-2 py-1  bg-blue-400 text-lg text-white font-semibold rounded  hover:bg-blue-500 hover:shadow-sm hover:translate-y-0.5 transform transition ">検索</button>
+  </form>
 
-    <a href="#" class="mr-4">
-      <i class="fa-solid fa-trash-can"></i>
-    </a>
-  </nav>
-</header>
+  <x-dropdown>
+    <x-slot name="trigger">
+      <div class="">
+        <img class="object-cover h-8 w-8 rounded-full overflow-hidden border-2 border-gray-600 focus:outline-none focus:border-white inline-block" src="" alt="ユーザーアイコン">
+        <i class="fa fa-caret-down inline-block align-middle"></i>
+      </div>
+    </x-slot>
+    <x-slot name="content">
+      <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">
+        ゴミ箱
+        <i class="fa-solid fa-trash-can"></i>
+      </a>
+      <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white">
+        ユーザー情報
+      </a>
+    </x-slot>
+  </x-dropdown>
+</nav>
