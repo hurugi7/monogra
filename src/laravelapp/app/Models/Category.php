e144e2435c0d5a33f6671ea67dd5a111c7c25bb1
hurugi7 <r.f.pockysakusaku@gmail.com>
2022-12-23 02:12:02 +0900

    リレーションを追記

--- src/laravelapp/app/Models/Category.php
+++ src/laravelapp/app/Models/Category.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\SubCategory;
+use App\Models\Item;
 
 class Category extends Model
 {
     use HasFactory;
+
+    public function subCategories()
+    {
+        return $this->hasMany(SubCategory::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
