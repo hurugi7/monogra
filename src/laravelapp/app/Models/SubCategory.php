418df139f2ccb41d966a096acf94352c55d08a1b
hurugi7 <r.f.pockysakusaku@gmail.com>
2022-12-23 02:12:38 +0900

    items, subCategoriesのモデル、コントローラーを新規作成

---
+++ src/laravelapp/app/Models/SubCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Item;
+
+class SubCategory extends Model
+{
+    use HasFactory;
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
+}
