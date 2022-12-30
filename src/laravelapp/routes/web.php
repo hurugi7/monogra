6851a326acf7dacc6a3b2b8e6ee1c57f85d38ab5
hurugi7 <r.f.pockysakusaku@gmail.com>
2022-12-31 01:41:40 +0900

    HaveSubCategoryControllerのuseを追記

--- src/laravelapp/routes/web.php
+++ src/laravelapp/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HaveCategoryController;
+use App\Http\Controllers\HaveSubCategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
