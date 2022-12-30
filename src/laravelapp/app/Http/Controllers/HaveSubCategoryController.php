3ad49599d6d23e6fcde0f48db32233f97ac783ac
hurugi7 <r.f.pockysakusaku@gmail.com>
2022-12-31 01:39:39 +0900

    indexメソッドを追加

--- src/laravelapp/app/Http/Controllers/HaveSubCategoryController.php
+++ src/laravelapp/app/Http/Controllers/HaveSubCategoryController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\SubCategory;
+use App\Models\Item;
 
 class HaveSubCategoryController extends Controller
 {
-    //
+    public function index(int $category, int $sub_category)
+    {
+        $categories = Category::all();
+
+        $current_category = Category::find($category);
+
+        $sub_categories = SubCategory::where('category_id', $current_category->id)->get();
+
+        return view('have_category_index',[
+            'categories' =>  $categories,
+            'current_category_id' => $current_category->id,
+            'sub_categories' => $sub_categories,
+        ]);
+    }
 }
