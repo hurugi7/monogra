74d0b991947e309fd7e1b478299726fef797ea2e
hurugi7 <r.f.pockysakusaku@gmail.com>
2022-12-31 01:37:30 +0900

    indexメソッドを削除
    表示させたいのはアイテムまで。このコントローラーのindexメソッドだとカテゴリーしか表示できない。

--- src/laravelapp/app/Http/Controllers/HaveCategoryController.php
+++ src/laravelapp/app/Http/Controllers/HaveCategoryController.php
@@ -8,18 +8,6 @@ use App\Models\SubCategory;
 
 class HaveCategoryController extends Controller
 {
-    public function index()
-    {
-        $user_1_categories = Category::where('user_id', 1)->get();
-
-        $sub_categories = SubCategory::whereIn('category_id', [1, 2, 3, 4, 5, 6, 7, 8])->get();
-
-        return view('have_category_index',[
-            'user_1_categories' =>  $user_1_categories,
-            'sub_categories' => $sub_categories,
-        ]);
-    }
-
     public function create()
     {
         return 'Hello, world!';
