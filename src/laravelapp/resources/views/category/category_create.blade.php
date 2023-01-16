986c88f62c97d595ff843b25811a86dcbccc55be
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-01-16 23:33:56 +0900

    入力値復元のためinput要素にvalue属性を追加

--- src/laravelapp/resources/views/category/category_create.blade.php
+++ src/laravelapp/resources/views/category/category_create.blade.php
@@ -7,12 +7,21 @@
       </a>
       <div class="py-3">カテゴリを追加する</div>
     </div>
+    @if($errors->any())
+      <div class="mx-8 my-3 p-3 bg-red-200 rounded">
+        <ul>
+          @foreach($errors->all() as $message)
+            <li class="text-red-600">{{ $message }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
     <form action="{{ route('have_category.store') }}" method="post" class="mx-8 my-3">
       @csrf
       <label for="category_name" class="block text-sm font-bold text-gray-900 mb-2">
         カテゴリ名
       </label>
-      <input type="text" class="block w-full p-4 mb-5 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500" name="category_name" id="category_name">
+      <input type="text" class="block w-full p-4 mb-5 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500" name="category_name" id="category_name" value="{{ old('category_name') }}">
       <div class="text-right">
         <a href="{{ route('have_category.store') }}">
           <button class="text-right bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">
