948f743b44d59aa531788d2e226568458f47cd86
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-01-17 16:31:01 +0900

    カテゴリ一覧の表示方法の変更と削除時の確認ダイアログの追加

--- src/laravelapp/resources/views/category/category_index.blade.php
+++ src/laravelapp/resources/views/category/category_index.blade.php
@@ -1,28 +1,83 @@
 <x-guest-layout>
   <x-header/>
   <div class="bg-white items-center mx-auto">
-    <div class="flex py-5 px-5 justify-between items-center bg-gray-200">
+    <div class="flex justify-between py-5 px-5 items-center bg-gray-200">
       <div>カテゴリ一覧</div>
-      <x-dropdown>
-        <x-slot name="trigger">
-          <a href="#" class="hover:bg-gray-400 font-bold py-1 px-2 rounded">
-            <i class="fa-solid fa-ellipsis-vertical fa-lg"></i>
-          </a>
-        </x-slot>
-        <x-slot name="content">
-          <a href="{{ route('have_category.create') }}" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white drop-shadow-lg">追加</a>
-          <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white drop-shadow-lg">削除</a>
-          <a href="#" class="block px-4 py-2 text-gray-800 hover:bg-indigo-500 hover:text-white drop-shadow-lg">編集</a>
-        </x-slot>
-      </x-dropdown>
-    </div>
-    @foreach($categories as $category)
-      <div class="bg-slate-100">
-        <a href="{{ route('have_sub_category.index', ['category' => $category->id]) }}" class="flex justify-between hover:bg-indigo-500 hover:text-white">
-          <div class="block text-lg py-3 px-5">{{ $category->category_name }}</div>
-          <div class="block text-lg py-3 px-5">{{ $category->sub_categories_count * $items[0]->items_count }}</div>
-        </a>
+      <div>
+        <a href="{{ route('have_category.create') }}" class="shadow-lg px-2 py-1  bg-yellow-300 text-white font-sm rounded  hover:bg-yellow-400 hover:shadow-sm hover:translate-y-0.5 transform transition">カテゴリ追加</a>
       </div>
-    @endforeach
+    </div>
+    <div class="flex flex-col">
+      <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-4 inline-block min-w-full sm:px-6 lg:px-8">
+          <div class="overflow-hidden">
+            <table class="min-w-full text-left">
+              <thead class="border-b bg-gray-100">
+                <tr>
+                  <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                    カテゴリ名
+                  </th>
+                  <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                    アイテム数
+                  </th>
+                  <th scope="col" colspan="3" class="text-sm font-medium text-gray-900 px-6 py-4">
+                    オプション
+                  </th>
+                </tr>
+              </thead class="border-b">
+              <tbody>
+                @foreach($categories as $category)
+                <tr class="bg-white border-b">
+                  <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                    {{ $category->category_name }}
+                  </td>
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    {{ $category->categories_count * $items[0]->items_count }}
+                  </td>
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    <a href="{{ route('have_sub_category.index', ['category' => $category->id]) }}">
+                      <button class="shadow-lg px-2 py-1  bg-green-300 text-white font-sm rounded  hover:bg-green-400 hover:shadow-sm hover:translate-y-0.5 transform transition ">
+                        サブカテゴリへ
+                      </button>
+                    </a>
+                  </td>
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    <a href="{{ route('have_category.edit', ['category' => $category->id]) }}">
+                      <button class="shadow-lg px-2 py-1  bg-blue-300 text-white font-sm rounded  hover:bg-blue-400 hover:shadow-sm hover:translate-y-0.5 transform transition ">
+                        編集
+                      </button>
+                    </a>
+                  </td>
+                  <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                    <div x-data="{ open: false }">
+                      <button class="shadow-lg px-2 py-1  bg-red-300 text-white font-sm rounded  hover:bg-red-400 hover:shadow-sm hover:translate-y-0.5 transform transition " @click="open = true">
+                        削除
+                      </button>
+                      <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center" style="background-color: rgba(0,0,0,.5);" x-show="open">
+                        <div class="text-left bg-white h-auto p-4 md:max-w-xl md:p-6 lg:p-8 shadow-xl rounded mx-2 md:mx-0" @click.away="open = false">
+                          <h2 class="text-2xl">削除の確認</h2>
+                          <p class="p-3">「{{ $category->category_name }}」を削除しますか？</p>
+                          <div class="flex justify-center mt-8">
+                            <button class="bg-gray-700 text-white px-4 py-2 rounded no-outline focus:shadow-outline select-none mx-3" @click="open = false">
+                              キャンセル
+                            </button>
+                            <form action="{{ route('have_category.destroy', ['category' => $category->id]) }}" method="post" class="mx-3">
+                              @csrf
+                              @method('delete')
+                              <input type="submit" value="削除" class="shadow-lg px-4 py-2  bg-red-300 text-white font-sm rounded  hover:bg-red-400 hover:shadow-sm hover:translate-y-0.5 transform transition">
+                            </form>
+                          </div>
+                          </div>
+                        </div>
+                      </div>
+                  </td>
+                </tr class="bg-white border-b">
+                @endforeach
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </x-guest-layout>
