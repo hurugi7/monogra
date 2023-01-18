7584cb4d36d5179561bdb22615aa467f7544568e
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-01-18 14:00:11 +0900

    indexメソッドの修正とcreate,storeメソッドの追加

--- src/laravelapp/app/Http/Controllers/HaveSubCategoryController.php
+++ src/laravelapp/app/Http/Controllers/HaveSubCategoryController.php
@@ -3,28 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateSubCategory;
 use App\Models\Category;
+use App\Models\User;
 use App\Models\SubCategory;
 use App\Models\Item;
+use Illuminate\Support\Facades\Auth;
 
 class HaveSubCategoryController extends Controller
 {
     public function index(int $category)
     {
-        $categories = Category::all();
-
         $current_category = Category::find($category);
 
-        $sub_categories = SubCategory::where('category_id', $current_category->id)->get();
-
-        $items = SubCategory::withCount('items')->get();
+        $sub_categories = SubCategory::where('category_id', $current_category->id)->withCount('items')->get();
 
         return view('sub_category.sub_category_index',[
-            'categories' =>  $categories,
             'current_category' => $current_category,
             'current_category_id' => $current_category->id,
             'sub_categories' => $sub_categories,
-            'items' => $items,
+        ]);
+    }
+
+    public function create(int $category)
+    {
+        $current_category = Category::find($category);
+
+        return view('sub_category.sub_category_create', [
+            'current_category_id' => $current_category->id,
+        ]);
+    }
+
+    public function store(CreateSubCategory $request, int $category)
+    {
+        $current_category = Category::find($category);
+
+        $sub_category = new SubCategory();
+
+        $sub_category->sub_category_name = $request->sub_category_name;
+
+        $current_category->subCategories()->save($sub_category);
+
+        return redirect()->route('have_sub_category.index', [
+            'category' => $current_category->id,
         ]);
     }
 }
