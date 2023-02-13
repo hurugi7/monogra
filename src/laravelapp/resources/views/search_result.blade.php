92020a3356302667a5431120bdba97146000bfc7
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-02-13 14:12:12 +0900

    検索結果画面のレイアウトの変更

--- src/laravelapp/resources/views/search_result.blade.php
+++ src/laravelapp/resources/views/search_result.blade.php
@@ -1,17 +1,35 @@
 <x-guest-layout>
-<x-header/>
-  <div class="">
+<x-header :user="$user"/>
+  <div class="mt-4">
     <div class="m-3 flex items-center">
-      <i class="fas fa-search mr-1"></i>
-      <div class="text-lg font-semibold">検索結果一覧</div>
+      <div class="mr-5">
+        <a href="{{ session('checkPointURL') }}">
+          <i class="fa fa-arrow-left" aria-hidden="true"></i>
+        </a>
+      </div>
+      <div class="text-xs sm:text-sm">検索結果一覧</div>
     </div>
-    <div class="">「{{ $search }}」の検索結果</div>
+    <div class="ml-2 text-sm sm:text-base">「{{ $search }}」の検索結果</div>
     <div class="">
       <div class="mt-2">
         @foreach($items as $item)
-          <a href="{{ route('have_item.show', ['category' => $item->subCategory->category_id, 'sub_category' => $item->sub_category_id, 'item' => $item->id]) }}">
-            <div class="">{{ $item->item_name }}</div>
-          </a>
+          <div class="hover:bg-slate-200">
+            <a href="{{ route('have_item.show', ['category' => $item->subCategory->category_id, 'sub_category' => $item->sub_category_id, 'item' => $item->id]) }}" class="block basis-11/12 px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-900">
+              <div class="flex justify-between items-center">
+                <div class="flex items-center">
+                  @if($item->photos()->where('item_id', $item->id)->exists())
+                    <img src="{{ asset('storage/' . $item->photos()->first()->path) }}" alt="" class="w-16 h-16 object-cover rounded-md border-2 mr-5">
+                  @else
+                    <img src="{{ asset('storage/20200505_noimage.png') }}" alt="" class="w-16 h-16 object-cover rounded-md border-2 mr-5">
+                  @endif
+                  <div class="text-lg font-medium">{{ $item->item_name }}</div>
+                </div>
+                <div>
+                  <i class="fa-solid fa-angle-right fa-lg"></i>
+                </div>
+              </div>
+            </a>
+          </div>
         @endforeach
       </div>
     </div>
