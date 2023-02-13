387f1788809a6d345e5f756bc5555896650c11f1
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-02-13 16:21:07 +0900

    アイテム写真のデフォルト写真の設定を削除

---
+++ src/laravelapp/database/migrations/2023_02_07_084556_remove_default_value_from_path_column_on_item_photos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveDefaultValueFromPathColumnOnItemPhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('item_photos', function (Blueprint $table) {
+            $table->dropColumn('path');
+        });
+
+        Schema::table('item_photos', function (Blueprint $table) {
+            $table->string('path')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('item_photos', function (Blueprint $table) {
+            //
+        });
+    }
+}
