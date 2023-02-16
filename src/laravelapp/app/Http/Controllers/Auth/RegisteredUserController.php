e68b1954f2d23132ffd6297a3e49fe658892f642
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-02-16 15:32:06 +0900

    オリジナル登録フォームの作成

--- src/laravelapp/app/Http/Controllers/Auth/RegisteredUserController.php
+++ src/laravelapp/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -20,7 +20,7 @@ class RegisteredUserController extends Controller
      */
     public function create()
     {
-        return view('auth.register');
+        return view('auth.original_register');
     }
 
     /**
