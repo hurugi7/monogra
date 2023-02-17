af9f63499b13506da6b64ffb106c01b38a88462a
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-02-17 11:50:34 +0900

    検索結果がない時の表示を実装

--- src/laravelapp/resources/views/search_result.blade.php
+++ src/laravelapp/resources/views/search_result.blade.php
@@ -10,27 +10,33 @@
       <div class="text-xs sm:text-sm">検索結果一覧</div>
     </div>
     <div class="ml-2 text-sm sm:text-base">「{{ $search }}」の検索結果</div>
-    <div class="">
+    <div class="ml-2">
       <div class="mt-2">
-        @foreach($items as $item)
-          <div class="hover:bg-slate-200">
-            <a href="{{ route('have_item.show', ['category' => $item->subCategory->category_id, 'sub_category' => $item->sub_category_id, 'item' => $item->id]) }}" class="block basis-11/12 px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-900">
-              <div class="flex justify-between items-center">
-                <div class="flex items-center">
-                  @if($item->photos()->where('item_id', $item->id)->exists())
-                    <img src="{{ asset('storage/' . $item->photos()->first()->path) }}" alt="" class="w-16 h-16 object-cover rounded-md border-2 mr-5">
-                  @else
-                    <img src="{{ asset('storage/20200505_noimage.png') }}" alt="" class="w-16 h-16 object-cover rounded-md border-2 mr-5">
-                  @endif
-                  <div class="text-lg font-medium">{{ $item->item_name }}</div>
+        @if(count($items) >= 1)
+          @foreach($items as $item)
+            <div class="hover:bg-slate-200">
+              <a href="{{ route('have_item.show', ['category' => $item->subCategory->category_id, 'sub_category' => $item->sub_category_id, 'item' => $item->id]) }}" class="block basis-11/12 px-4 py-2 whitespace-nowrap text-sm font-medium text-gray-900">
+                <div class="flex justify-between items-center">
+                  <div class="flex items-center">
+                    @if($item->photos()->where('item_id', $item->id)->exists())
+                      <img src="{{ asset('storage/' . $item->photos()->first()->path) }}" alt="" class="w-16 h-16 object-cover rounded-md border-2 mr-5">
+                    @else
+                      <img src="{{ asset('storage/20200505_noimage.png') }}" alt="" class="w-16 h-16 object-cover rounded-md border-2 mr-5">
+                    @endif
+                    <div class="text-lg font-medium">{{ $item->item_name }}</div>
+                  </div>
+                  <div>
+                    <i class="fa-solid fa-angle-right fa-lg"></i>
+                  </div>
                 </div>
-                <div>
-                  <i class="fa-solid fa-angle-right fa-lg"></i>
-                </div>
-              </div>
-            </a>
+              </a>
+            </div>
+          @endforeach
+        @else
+          <div class="mt-3">
+          「{{ $search }}」に一致するアイテムはみつかりませんでした。
           </div>
-        @endforeach
+        @endif
       </div>
     </div>
   </div>
