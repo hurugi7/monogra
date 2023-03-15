e1d9c7c11223be812856f0953b4f7ad08a1e6ae2
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-03-15 12:07:02 +0900

    composerインストールの場所を変更

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-cd var/www/html/laravelapp
-composer install
-
 # マイグレーションを実行する前にファイルが存在するかどうかを確認する
 if [ ! $(php artisan migrate:status | grep -c "^\| Y ") -eq $(php artisan migrate:status | grep -c "^\| N ") ]; then
+  cd var/www/html/laravelapp
   php artisan migrate --force
   touch /var/www/html/laravelapp/.migrated
 fi
