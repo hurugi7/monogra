b0941c8426f484f690d68bd57252270c28ffc251
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-03-24 20:49:31 +0900

    favconの追加

--- src/laravelapp/resources/views/layouts/guest.blade.php
+++ src/laravelapp/resources/views/layouts/guest.blade.php
@@ -16,6 +16,7 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
 
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
