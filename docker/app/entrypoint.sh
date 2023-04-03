29f479354ee424f10f7370b6028132b3e25e032a
hurugi7 <r.f.pockysakusaku@gmail.com>
2023-04-03 19:12:42 +0900

    常にマイグレーションをするように変更
    新しいマイグレーションファイルが実行されるようにするため

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# マイグレーションを実行する前にファイルが存在するかどうかを確認する
-if [ ! $(php artisan migrate:status | grep -c "^\| Y ") -eq $(php artisan migrate:status | grep -c "^\| N ") ]; then
-  cd /var/www/html/laravelapp
-  php artisan migrate --force
-  touch /var/www/html/laravelapp/.migrated
-fi
+cd /var/www/html/laravelapp
+php artisan migrate --force
 
 /usr/sbin/apache2ctl -D FOREGROUND
